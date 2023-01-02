9d8c2059c677097c2125b077ad176b749c7cfd40
Grant Hale <granthale10@gmail.com>
2023-01-01 19:59:31 -0600

    Updated code.js and manifest.json

--- Code.js
+++ Code.js
@@ -1,18 +1,33 @@
-function onLoad() {
-  var mode = getQueryParameter('mode');
-  if (mode == 'other') {
-    // Initialize sidebar in a different context
+// placeholder
+function onDefaultHomePageOpen() {
+  return
+}
+
+// Function for when calendar is initially opened
+function onCalendarHomePageOpen() {
+  // Get the default calendar and events for the current day
+  var calendar = CalendarApp.getDefaultCalendar();
+  var events = calendar.getEventsForDay(new Date());
+
+  // Check if there are any events today
+  if (events.length > 0) {
+    // If there are events, show a message in the sidebar
+    var html = HtmlService.createHtmlOutput("You have " + events.length + " events today.");
+    CalendarApp.getDefaultCalendar().showSidebar(html);
   } else {
-    // Assumes that sidebar is defined in the HTML document as: <div id="sidebar"></div>
-    var sidebarElement = document.querySelector('#sidebar');
-    // Retrieves the currently selected event
-    var event = Calendar.getEventById(Calendar.getEventId());
-    var title = event.getTitle();
-    sidebarElement.innerHTML = '<h1>' + title + '</h1>';
+    // If there are no events, show a message in the sidebar
+    var html = HtmlService.createHtmlOutput("You have no events today. Enjoy your day!");
+    CalendarApp.getDefaultCalendar().showSidebar(html);
   }
 }
 
-function getQueryParameter(name) {
-  var url = new URL(window.location.href);
-  return url.searchParams.get(name);
+// The include function allows you to include the contents of another HTML file in the sidebar. You can use this to structure your sidebar content into multiple HTML files for easier organization.
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+      .getContent();
+}
+
+// TODO - to use for when calendar event is opened
+function onCalendarEventOpen() {
+
 }
